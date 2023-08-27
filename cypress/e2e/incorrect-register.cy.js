765a61e83a06ee7c5a3aa0aadc21fb64951cf272
vique <victoriarduarte@hotmail.com>
2023-08-27 19:15:14 -0300

    Update element locator

--- cypress/e2e/incorrect-register.cy.js
+++ cypress/e2e/incorrect-register.cy.js
@@ -1,9 +1,9 @@
 describe('Register page', () => {
   it('Fill out form fields incorrectly', () => {
     cy.visit('http://localhost:4200/#/home')
-    cy.contains('a', 'Register now').click();
-    cy.contains('button', 'Register').click();
-    cy.contains('button', 'Register').click();
+    cy.get('[data-test="register"]').click();
+    cy.get('[data-test="btnRegister"]').click();
+    cy.get('[data-test="btnRegister"]').click();
     cy.contains('Email is required!').should('be.visible');
     cy.contains('Full name is required!').should('be.visible');
     cy.contains('User name is required!').should('be.visible');
