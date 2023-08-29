f99f38b4733d3e556fb5dac1f4a9dff520050656
vique <victoriarduarte@hotmail.com>
2023-08-29 16:44:08 -0300

    Add 'login' command

--- cypress/e2e/correct-login.cy.js
+++ cypress/e2e/correct-login.cy.js
@@ -1,8 +1,9 @@
 describe('Login page', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/#/home') })
+
   it('Fill out form fields correctly to login', () => {
-    cy.visit('http://localhost:4200/#/home')
-    cy.get('[data-test="loginUserName"]').type('victoriaduarte');
-    cy.get('[data-test="loginPassword"]').type('Victoria123');
-    cy.contains('button', 'login').click();
+    cy.login('victoriaduarte','Victoria123')
   })
 })
