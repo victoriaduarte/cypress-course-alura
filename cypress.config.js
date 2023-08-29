4fab9be042ded7c85ccd81e325f754761a76fd76
vique <victoriarduarte@hotmail.com>
2023-08-29 16:45:44 -0300

    Configure reporter mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      { "reporter"; "mochawesome", 
+        "reporterOptions"; 
+          { "reportDir"; "cypress/report/mochawesome-report", 
+           "overwrite"; false, 
+           "html"; true, 
+           "json"; false, 
+           "timestamp"; "mmddyyyy_HHMMss" }}
     },
   },
 };
