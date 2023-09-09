8200d62e75bb6fb5ece52670a8e99e8177ebc49c
vique <victoriarduarte@hotmail.com>
2023-09-09 17:26:04 -0300

    Remove quotes around parameter

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 
 Cypress.Commands.add('login', (user, password) => {
-    cy.get('[data-test="loginUserName"]').type('user');
-    cy.get('[data-test="loginPassword"]').type('password');
+    cy.get('[data-test="loginUserName"]').type(user);
+    cy.get('[data-test="loginPassword"]').type(password);
     cy.contains('button', 'login').click();
 })
