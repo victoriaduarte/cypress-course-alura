95e73c267c2aec2bb713e1737268b25aafe9e642
vique <victoriarduarte@hotmail.com>
2023-09-09 16:35:32 -0300

    Update incorrect-login

--- cypress/e2e/incorrect-login.cy.js
+++ cypress/e2e/incorrect-login.cy.js
@@ -1,7 +1,21 @@
 describe('Login page', () => {
-  it('Verify required field validation messages', () => {
+
+  beforeEach(() => {
     cy.visit('http://localhost:4200/#/home')
-    cy.contains('User name is required!').should('be.visible');
-    cy.contains('Password is required!').should('be.visible');
+
+    cy.intercept('POST', 'http://localhost:3000/user/login', {
+      statusCode: 400
+    }).as('stubPost')
+
   })
+    
+    it('Verify required field validation messages', () => {
+      cy.contains('User name is required!').should('be.visible');
+      cy.contains('Password is required!').should('be.visible');
+    })
+
+    it('Should fail even if the fields are filled properly', () => {
+      cy.login('victoriaduarte','Victoria123')
+      cy.wait('@stubPost')
+    })
 })
